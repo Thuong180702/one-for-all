f5d55116882c8946cbe04b8f4bd4e22900f5a3ee
thuong180702 <vanthuong180702t@gmail.com>
2026-08-07 15:11:06 +0700

    chore: bump version to v0.8.0

--- Casks/one-for-all.rb
+++ Casks/one-for-all.rb
@@ -1,6 +1,6 @@
 cask "one-for-all" do
-  version "0.7.0"
-  sha256 "9a5caf25cd0caa2bc5fedee80e2f010b644996c3a47e5586b6e0c86f01d3a378"
+  version "0.8.0"
+  sha256 "1faaf6a6b65b334723a362f53c44cce812012ba324d2feb48b2d6ebc26edf23a"
 
   url "https://github.com/Thuong180702/one-for-all/releases/download/v#{version}/one-for-all-v#{version}-mac.zip"
   name "one-for-all"
